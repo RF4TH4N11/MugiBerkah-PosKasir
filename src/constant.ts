c636fbcd2878001f9508feb4b2cba11604335dcd
RF4TH4N11 <almujahid1204@gmail.com>
2025-10-20 14:17:49 +0700

    activated Base URL

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:3000";
-// export const BASE_URL = "https://mugiberkah.famsy.my.id";
+// export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://mugiberkah.famsy.my.id";
